38a2a270c66942b5434129ffd57da6ff630fdfce
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2025-06-03 13:37:21 +0530

    bind2uio.sh: validity check for device; better comments; another example device in comments

--- ch9/bind2uio.sh
+++ ch9/bind2uio.sh
@@ -4,11 +4,27 @@
 #  and it's [VID,PID] is [1af4,1003]
 
 #--- KEEP UPDATED ---
-PCI_DEVNAME=virtio
+PCI_DEVNAME="virtio"
 PCI_DRIVER=virtio-pci
 PCI_BDF="00:05.0"
-PCI_VID_PID="1af4 1003"
+PCI_VID_PID="1af4:1003"
 
+# As another example, the Ethernet controller PCI device
+# NOTE:
+# - Doing this on an existing 'live' device can be dangerous! It will almost
+#   certainly cause it to stop functioning or misbehave... (it's why we only
+#   run this experiment on a Qemu-emulated 'dummy' PCIe device, one we aren't
+#   actively using)
+# - Update the below values to reflect lspci -nnv output on your box
+#PCI_DEVNAME="Ethernet controller"
+#PCI_DRIVER=e1000
+#PCI_BDF="00:03.0"
+#PCI_VID_PID="8086:100e"
+
+die()
+{
+echo >&2 "FATAL: $*" ; exit 1
+}
 runcmd()
 {
 	[[ $# -eq 0 ]] && return
@@ -17,14 +33,23 @@ runcmd()
 	echo
 }
 
+#--- 'main'
+[[ $(id -u) -ne 0 ]] && die "Need root"
+hash lspci || die "Install lspci util"
+# Does the PCI device exist?
+lspci -nn | grep "${PCI_BDF}.*${PCI_VID_PID}" >/dev/null || die "No PCI device matching address \"${PCI_BDF}\" and [VID:PID] \"${PCI_VID_PID}\""
+
 # reset
-runcmd "echo -n "0000:${PCI_BDF}" > /sys/bus/pci/drivers/uio_pci_generic/unbind"
+runcmd "echo -n "0000:${PCI_BDF}" > /sys/bus/pci/drivers/uio_pci_generic/unbind 2>/dev/null"
 
 runcmd "lspci -nnk|grep -i -A3 ${PCI_DEVNAME}"
-runcmd "modprobe uio_pci_generic"
+runcmd "modprobe uio_pci_generic 2>/dev/null"
 
-runcmd "echo "${PCI_VID_PID}" > /sys/bus/pci/drivers/uio_pci_generic/new_id"
+PCI_VID_PID_SPACESEP=$(echo ${PCI_VID_PID} | sed -e "s/:/ /")
+runcmd "echo "${PCI_VID_PID_SPACESEP}" > /sys/bus/pci/drivers/uio_pci_generic/new_id"
+# Unbind PCI device from the original driver
 runcmd "echo -n "0000:${PCI_BDF}" > /sys/bus/pci/drivers/${PCI_DRIVER}/unbind"
+# Bind PCI device to the kernel uio_pci_generic driver
 runcmd "echo -n "0000:${PCI_BDF}" > /sys/bus/pci/drivers/uio_pci_generic/bind"
 
 # Verify
