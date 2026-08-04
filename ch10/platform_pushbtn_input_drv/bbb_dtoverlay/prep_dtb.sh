80a48b94b89facdaeb3a8eceb6b8acea7c5b2217
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2026-08-04 17:37:52 +0530

    prep_dtb.sh: initial commit

---
+++ ch10/platform_pushbtn_input_drv/bbb_dtoverlay/prep_dtb.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Part of the LDDIA book's source code.
+# Location: ch10/platform_pushbtn_input_drv/bbb_dtoverlay
+# Wrapper script to help setup the DT overlay -to- DTB for the TI BBB.
+set -euo pipefail
+
+KPFX=~/6.18.33
+echo "cp gpio_btn_bbb.dts ${KPFX}/arch/arm/boot/dts/ti/omap/"
+cp gpio_btn_bbb.dts ${KPFX}/arch/arm/boot/dts/ti/omap/
+
+echo "
+Now head over to ${KPFX}, build the DTB:
+ make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabihf- dtbs
+
+Once that's done, ensure the BBB's ready and then
+press [Enter] to continue here...
+"
+read x
+
+ls -l ${KPFX}/arch/arm/boot/dts/ti/omap/gpio_btn_bbb.dtb
+BBB_IP=192.168.0.30   # update for your board!
+DEST_BBB=/boot/dtbs/6.18.39-bone44/overlays # can't directly scp here: Permission denied (even w/ sudo)
+echo "scp ${KPFX}/arch/arm/boot/dts/ti/omap/gpio_btn_bbb.dtb  debian@${BBB_IP}:~/"
+scp ${KPFX}/arch/arm/boot/dts/ti/omap/gpio_btn_bbb.dtb  debian@${BBB_IP}:~/
+
+echo "Done; now:
+ reboot the BBB
+ sudo cp ~/gpio_btn_bbb.dtb ${DEST_BBB}/BBB-GPIO-BTN.dtbo ; sync
+ and test."
